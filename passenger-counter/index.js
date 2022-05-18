2bbbbc55f841fc8847b16ac5367cb1da5c2516dd
josephndegwa <kariukindegwa98@gmail.com>
2022-05-18 21:19:39 +0300

    add save counter

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,15 +1,22 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el") //pass  in argument
-console.log(countEl)
 
 let count = 0
 
+console.log(saveEl)
+
 function increment() {
-    count = count + 1
-    countEl.innerText = count
-    console.log(count)
+    count += 1
+    countEl.textContent = count
+    
 }
 
 
 function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
     console.log(count)
+    countEl.textContent = 0
+    count = 0
 }
+
